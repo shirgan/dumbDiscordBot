7099966370020118c466132bb22943abbce4ea43
ConnorH <connorchollis@gmail.com>
2017-11-15 17:12:02 -0800

    removing empty comments

--- src/client/index.js
+++ src/client/index.js
@@ -3,32 +3,12 @@ import fs from 'fs';
 import path from 'path';
 
 const clientController = (mediator, discordClient) => {
-
-  // images
-  let avatarImagesPath = path.join(__dirname, '../assets/avatars');
-
-  const generateSoundFileList = (dir) => {
-    return fs.readdirSync(dir)
-    .map(file => {
-      return path.join(dir, file);
-    }); 
-  };
-
-  let departureImageFiles = generateSoundFileList(avatarImagesPath);
   
   const setClientSettings = (options) => {
-    
-    //const {} = options;
     return new Promise((resolve, reject) => {   // lol
-      discordClient.user.setUsername('Dumb Discord Bot');
+      discordClient.user.setUsername('Dumber Discord Bot');
       discordClient.user.setGame('your favorite memes since 2017');
-      // discordClient.user.setAvatar(getRandomFile(departureImageFiles));
-
     });
-  };
-  
-  let getRandomFile = (items) => {
-    return items[Math.floor(Math.random()*items.length)];
   };
 
   return Object.create({
