bf97c61e627a9a287d0f2bcffe2f6960705574fe
Gregory Brewster <gbrewster@ci.sandy.or.us>
2018-08-12 19:55:14 -0700

    Added some test cases

--- src/config/discord/discordCfgIdx.spec.js
+++ src/config/discord/discordCfgIdx.spec.js
@@ -1,39 +1,63 @@
-// 'use strict';
+'use strict';
 
-// const {EventEmitter} = require('events');
-// import * as discordFile from './index';
+const {EventEmitter} = require('events');
+import * as discordFile from './index';
 
-// describe('Discord Config test', () => {
-//   let mediatorObj = new EventEmitter;
+describe('Discord Config test', () => {
   
-//   it("Discord connect const should be a function", () => {
-//     expect(discordFile.connect).toEqual(jasmine.any(Function));
-//   });
+  it("Discord connect const should be a function", () => {
+    expect(discordFile.connect).toEqual(jasmine.any(Function));
+  });
   
   
-//   describe('Check discord login', () => {
-//     let options, client, spyA;
-//     beforeEach(() => {
-//       options = {
-//         discordClientId: "asdf"
-//       };
-//       spyA = jasmine.createSpy('A');
-//       client = discordFile.connect(options, mediatorObj);
-//     });
+  describe('Check discord login', () => {
+    let options, client, spyA, mediatorObj;
+    beforeEach(() => {
+      mediatorObj = new EventEmitter;
+      spyA = jasmine.createSpy('A');
+    });
     
-//     it("Login should fail to login if no valid key is provided", (done) => {
-//       mediatorObj.on('boot.ready', spyA);
-//       mediatorObj.emit('boot.ready');
-//       expect(spyA).toHaveBeenCalled();
+    it("should fail to login if client key is no accepted from discord", (done) => {
+      options = {
+        discordClientId: 'asdf'
+      };
+      client = discordFile.connect(options, mediatorObj);
+      mediatorObj.on('boot.ready', spyA);
+      mediatorObj.emit('boot.ready');
+      expect(spyA).toHaveBeenCalled();
       
-//       mediatorObj.on('discord.error', (result) => {
-//         done();
-//       });
-//     });
-    
-//     //it("Login should succeed if a valid key is present", (done) => {
-      
-//     //});
+      mediatorObj.on('discord.error', (result) => {
+        done();
+      });
+    });
 
-//   });
-// });
+    it("should fail when no client key is provided", (done) => {
+      options = {
+        discordClientId: false // set to null to simulate a bad value
+      };
+      mediatorObj.on('discord.error', (result) => {
+        expect(result).toEqual('Client ID not found!');
+        done();
+      });
+
+      client = discordFile.connect(options, mediatorObj);
+      mediatorObj.on('boot.ready', spyA);
+      mediatorObj.emit('boot.ready');
+      expect(spyA).toHaveBeenCalled();
+
+    });
+
+    // BELOW NOT POSSIBLE WITHOUT DI 
+    // it("Login should succeed with mock", (done) => {
+    //   mediatorObj.on('boot.ready', spyA);
+    //   mediatorObj.emit('boot.ready');
+    //   expect(spyA).toHaveBeenCalled();
+
+    //   mediatorObj.on('discord.ready', (result) => {
+    //     done();
+    //   });
+
+    // });
+
+  });
+});
